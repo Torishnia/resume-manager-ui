99471fa8f9a410d9e9f36efb4ca2855a70d45ebc
Valeri <valeri.torishnya@gmail.com>
2024-01-14 19:25:12 +0200

    init project

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import('../views/CreateResume.vue')
+  },
+  {
+    path: '/view',
+    name: 'view',
+    component: () => import('../views/ViewResume.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
