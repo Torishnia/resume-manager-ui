7047d9b230d8529f0db41e97d7e0ad78530efdaa
Valeri <valeri.torishnya@gmail.com>
2024-01-18 23:50:30 +0200

    create form for create resume (part 1)

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="wrapper">
+    <button @click="toggleInfo" type="button" class="btn-details">
+      Contacts
+      <span>&#9650;</span>
+    </button>
+    <div v-if="showInfo">
+      <div class="contact-info">
+        <label for="phone">Phone</label>
+        <input
+          type="text"
+          name="phone"
+          v-model="phone"
+          maxlength="12"
+          @input="contactData"
+        />
+      </div>
+      <div class="contact-info">
+        <label for="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          v-model="email"
+          maxlength="64"
+          @input="contactData"
+        />
+      </div>
+      <div class="contact-info">
+        <label for="linkedIn">LinkedIn</label>
+        <input
+          type="text"
+          name="linkedIn"
+          v-model="linkedInURL"
+          maxlength="128"
+          @input="contactData"
+        />
+      </div>
+      <div class="contact-info">
+        <label for="telegram">Telegram</label>
+        <input
+          type="text"
+          name="telegram"
+          v-model="telegramURL"
+          maxlength="128"
+          @input="contactData"
+        />
+      </div>
+      <div class="contact-info">
+        <label for="gitHub">GitHub</label>
+        <input
+          type="text"
+          name="gitHub"
+          v-model="gitHubURL"
+          maxlength="128"
+          @input="contactData"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showInfo: false,
+        phone: '',
+        email: '',
+        linkedInURL: '',
+        telegramURL: '',
+        gitHubURL: '',
+      };
+    },
+    methods: {
+      toggleInfo() {
+        console.log(this.showInfo);
+        this.showInfo = !this.showInfo;
+      },
+      contactData() {
+        console.log('contactData');
+        this.$emit('contact-data', {
+          phone: this.phone,
+          email: this.email,
+          linkedInURL: this.linkedInURL,
+          telegramURL: this.telegramURL,
+          gitHubURL: this.gitHubURL,
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .wrapper {
+    margin: 30px 0;
+  }
+
+  .btn-details {
+    font-size: 18px;
+    font-weight: 500;
+    color: #2e3c51;
+    border: none;
+    cursor: pointer;
+  }
+
+  .contact-info {
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 5px;
+    font-size: 16px;
+    font-weight: 300;
+    color: #2e3c51;
+  }
+
+  input, textarea {
+    padding: 5px 10px;
+    font-size: 16px;
+    font-weight: 500;
+    border: 1px solid #2e3c51;
+    border-radius: 10px;
+    color: #2e3c51;
+  }
+</style>
--- src/views/CreateResume.vue
+++ src/views/CreateResume.vue
@@ -1,5 +1,179 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <form class="container" @submit.prevent="onSubmit">
+    <div class="info">
+      <label for="firstName">First Name</label>
+      <input
+        type="text"
+        name="firstName"
+        v-model="firstName"
+        maxlength="64"
+        required
+      />
+    </div>
+    <div class="info">
+      <label for="lastName">Last Name</label>
+      <input
+        type="text"
+        name="lastName"
+        v-model="lastName"
+        maxlength="64"
+        required
+      />
+    </div>
+    <div class="info">
+      <label for="age">Age</label>
+      <input
+        type="number"
+        name="age"
+        v-model="age"
+        required
+      />
+    </div>
+    <div class="info">
+      <label for="location">Location</label>
+      <input
+        type="text"
+        name="location"
+        v-model="location"
+        maxlength="128"
+        required
+      />
+    </div>
+    <div class="info">
+      <label for="position">Position</label>
+      <input
+        type="text"
+        name="position"
+        v-model="position"
+        maxlength="128"
+        required
+      />
+    </div>
+    <div class="info">
+      <label for="description">Description*</label>
+      <textarea
+        type="text"
+        name="description"
+        v-model="description"
+        maxlength="128"
+      ></textarea>
+    </div>
+    <div class="info">
+      <label for="interests">Interests*</label>
+      <textarea
+        type="text"
+        name="interests"
+        v-model="interests"
+        maxlength="500"
+      ></textarea>
+    </div>
+    <ContactInfo @contact-data="handleContactData" />
+    <button type="submit">Create</button>
+  </form>
 </template>
+
+<script>
+  import ContactInfo from '@/components/ContactInfo.vue';
+
+  export default {
+    name: 'CreateResume',
+    data() {
+      return {
+        firstName: '',
+        lastName: '',
+        age: null,
+        location: '',
+        position: '',
+        description: '',
+        interests: '',
+        contact: {
+          phone: '',
+          email: '',
+          linkedInURL: '',
+          telegramURL: '',
+          gitHubURL: '',
+        }
+      };
+    },
+    components: { ContactInfo },
+    methods: {
+      handleContactData(data) {
+        console.log('Data', data);
+        this.contact = data;
+      },
+      async createResume() {
+        try {
+          const response = await fetch('https://resume-manager-api-git-main-torishnia.vercel.app/resumes/create', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                firstName: this.firstName,
+                lastName: this.lastName,
+                age: this.age,
+                location: this.location,
+                positionTitle: this.position,
+                positionDescription: this.description,
+                interests: this.interests,
+                contact: this.contactInfo,
+              })
+          });
+          if (!response.ok) throw new Error('Error creating resume');
+          console.log('Resume created successfully');
+        }
+        catch (err) {
+          console.error('Error creating resume:', err);
+        }
+      },
+      onSubmit() {
+        this.createResume();
+        this.firstName = '';
+        this.lastName = '';
+        this.age = null;
+        this.location = '';
+        this.position = '';
+        this.description = '';
+        this.interests = '';
+        this.contact = {
+          phone: '',
+          email: '',
+          linkedInURL: '',
+          telegramURL: '',
+          gitHubURL: '',
+        };
+      },
+  },
+}
+
+</script>
+
+<style scoped>
+  .container {
+    padding: 15px;
+    border: 1px solid red;
+  }
+
+  .info {
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 5px;
+    font-size: 16px;
+    font-weight: 300;
+    color: #2e3c51;
+  }
+
+  input, textarea {
+    padding: 5px 10px;
+    font-size: 16px;
+    font-weight: 500;
+    border: 1px solid #2e3c51;
+    border-radius: 10px;
+    color: #2e3c51;
+  }
+
+</style>
