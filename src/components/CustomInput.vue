50efd96d88788b75d71cd2780179b84ab354ed1b
Valeri <119043316+Torishnia@users.noreply.github.com>
2024-01-22 17:30:30 +0200

    4: update CreateResume/create CustomInput and CustomTextarea components (#9)

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="input-block">
+    <label :for=inputId>{{inputLabel}}</label>
+    <input
+      :id=inputId
+      :type=inputType
+      :name=inputName
+      :required=inputRequire
+      :maxlength=inputLength
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CustomInput',
+    props: [
+      'inputId',
+      'inputLabel',
+      'inputType',
+      'inputName',
+      'inputRequire',
+      'inputLength',
+      'modelValue',
+    ],
+  }
+</script>
+
+<style scoped>
+  .input-block {
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 5px;
+    font-size: 16px;
+    font-weight: 300;
+    color: #2e3c51;
+  }
+
+  input {
+    padding: 5px 10px;
+    font-size: 16px;
+    font-weight: 500;
+    border: 1px solid #2e3c51;
+    border-radius: 10px;
+    color: #2e3c51;
+  }
+</style>
