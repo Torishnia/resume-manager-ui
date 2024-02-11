cbb00d378900e3eee686d3f7cd15b92d72cc63b7
Valeri <valeri.torishnya@gmail.com>
2024-02-11 17:10:49 +0200

    16: create component CustomButton

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button type="button" class="btn-add" @click="$emit('click')">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: String,
+    },
+  }
+</script>
+
+<style scoped>
+  .btn-add {
+    margin-top: 14px;
+    padding: 5px;
+    height: 30px;
+    width: 120px;
+    font-weight: 600;
+    border-radius: 10px;
+    border: 2px solid #2e3c51;
+    background: #2e3c51;
+    color: white;
+    cursor: pointer;
+  }
+
+  .btn-add:hover {
+    background: none;
+    color: #2e3c51;
+  }
+</style>
